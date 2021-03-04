e25bd9cd784c6bc56927ece8489d1f58a96a3e16
brunoemferreira <brunoemf@gmail.com>
2021-03-04 14:39:41 -0300

    Criação das Rotas dos Usuários

--- api/routes/api/v1/index.js
+++ api/routes/api/v1/index.js
@@ -1,4 +1,6 @@
 // Aqui é onde ficam as rotas de todos os modelos do servidor
 const router = require("express").Router();
 
+router.use("/usuarios", require("./usuarios"));
+
 module.exports = router;
