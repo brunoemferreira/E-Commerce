ded7dec0e2196d5f756758a38b80c99a92206c4e
brunoemferreira <brunoemf@gmail.com>
2021-03-04 14:28:52 -0300

    Criando os métodos dentro da model de usuários

--- api/models/usuario.js
+++ api/models/usuario.js
@@ -39,6 +39,60 @@ const UsuarioSchema = new mongoose.Schema({
     },
     default: {}
   }
-}, {
-  timestamps: true
-})
+}, { timestamps: true });
+
+// Ativar o plugin do unique Validator para os campos que estão utilizando unique
+UsuarioSchema.plugin(uniqueValidator, { message: defaultMessages["parcial-utilizado"] })
+
+// ******** Métodos para a parte de usuário *********
+// Método para criação de nova senha de usuário
+UsuarioSchema.methods.setSenha = function (password) {
+  this.salt = crypto.randomBytes(16).toString("hex");
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, "sha512").toString("hex");
+};
+
+// Método para verificar validade da senha 
+UsuarioSchema.methods.validarSenha = function (password) {
+  const hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, "sha512").toString("hex");
+  return hash === this.hash;
+}
+
+UsuarioSchema.methods.gerarToken = function () {
+  const hoje = new Date();
+  const exp = new Date(today);
+  exp.setDate(today.getDate() + 15);
+
+  return jwt.sign({
+    id: this._id,
+    email: this.email,
+    nome: this.nome,
+    exp: parseFloat(exp.getTime() / 1000, 10)
+  }, secret);
+};
+
+UsuarioSchema.methods.enviarAuthJSON = function () {
+  return {
+    _id: this._id,
+    nome: this.nome,
+    email: this.email,
+    loja: this.loja,
+    role: this.permissao,
+    token: this.gerarToken()
+  };
+};
+
+// Recuperação de Senha
+UsuarioSchema.methods.criarTokenRecuperacaoSenha = function () {
+  this.recovery = {};
+  this.recovery.token = crypto.randomBytes(16).toString("hex");
+  this.recovery.date = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+  return this.recovery;
+};
+
+// Finalizar a Token após a recuperação da mesma
+UsuarioSchema.methods.finalizarTokenRecuperacaoSenha = function () {
+  this.recovery = { token: null, date: null };
+  return this.recovery;
+}
+
+module.exports = mongoose.model("Usuario", UsuarioSchema);
