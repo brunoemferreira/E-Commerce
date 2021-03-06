1497b4dab7cee4305123465b1486c132a01c2e20
brunoemferreira <brunoemf@gmail.com>
2021-03-06 09:13:52 -0300

    Criando na controller o recovery do usuario :  // GET /recuperar-senha  // POST /recuperar-senha // GET /senha-recuperada // POST /senha-recuperada

--- api/controllers/UsuarioController.js
+++ api/controllers/UsuarioController.js
@@ -35,6 +35,8 @@ class UsuarioController {
   store(req, res, next) {
     const { nome, email, password } = req.body;
 
+    if (!nome || !email || !password) return res.status(402).json({ errors: defaultMessages["fill-in-all-registration-fields"] });
+
     const usuario = new Usuario({ nome, email });
     usuario.setSenha(password);
 
@@ -78,6 +80,60 @@ class UsuarioController {
       if (!usuario.validarSenha(password)) return res.status(401).json({ errors: defaultMessages["invalid-password"] })
       return res.json({ usuario: usuario.enviarAuthJSON() })
     }).catch(next)
+  }
+
+
+  // RECOVERY
+
+  // GET /recuperar-senha
+  showRecovery(req, res, next) {
+    return res.render('recovery', { error: null, success: null });
+  }
+
+
+  // POST /recuperar-senha
+  createRecovery(req, res, next) {
+    const { email } = req.body;
+    // Verifica se o email foi preenchido
+    if (!email) return res.render('recovery', { error: defaultMessages["fill-in-email"], success: null });
+
+    // Verifica se o usuário existe
+    Usuario.findOne({ email }).then((usuario) => {
+      if (!usuario) return res.render('recovery', { error: defaultMessages["user-email-not-exist"], success: null });
+      const recoveryData = usuario.criarTokenRecuperacaoSenha();
+      return usuario.save().then(() => {
+        return res.render("recovery", { error: null, success: true });
+      }).catch(next);
+    }).catch(next);
+  }
+
+  // GET /senha-recuperada
+  showCompleteRecovery(req, res, next) {
+    if (!req.query.token) return res.render("recovery", { error: defaultMessages["undentified-token"], success: null });
+    Usuario.findOne({ "recovery.token": req.query.token }).then(usuario => {
+      if (!usuario) return res.render('recovery', { error: defaultMessages["user-email-not-exist"], success: null });
+      if (new Date(usuario.recovery.date) < new Date()) return res.render("recovery", { error: defaultMessages["expired-token"], success: null });
+      return res.render("recovery/store", { error: null, success: null, token: req.query.token })
+    }).catch(next);
+  }
+
+  // POST /senha-recuperada
+  completeRecovery(req, res, next) {
+    const { token, password } = req.body;
+    if (!token || !password) return res.render("recovery/store", { error: defaultMessages["fill-in-new-password"], success: null, token: token });
+    Usuario.findOne({ "recovery.token": token }).then(usuario => {
+      if (!usuario) return res.render("recovery", { error: defaultMessages["user-undentified"], success: null });
+
+      usuario.finalizarTokenRecuperacaoSenha();
+      usuario.setSenha(password);
+      return usuario.save().then(() => {
+        return res.render("recovery/store", {
+          error: null,
+          success: defaultMessages["password-change-sucefully"],
+          token: null
+        });
+      }).catch(next);
+    })
   }
 
 
