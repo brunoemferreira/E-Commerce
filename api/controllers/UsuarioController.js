343c1c09d7fd44a2034b9d5c9ce09dca649d8877
brunoemferreira <brunoemf@gmail.com>
2021-03-06 08:36:19 -0300

    Criando na controller as funcionalidades de de delete e de login do usuario

--- api/controllers/UsuarioController.js
+++ api/controllers/UsuarioController.js
@@ -43,11 +43,42 @@ class UsuarioController {
       .catch(next);
   }
 
+  // PUT
+  update(req, res, next) {
+    const { nome, email, password } = req.body;
+    Usuario.findById(req.payload.id)
+      .then((usuario) => {
+        if (!usuario) return res.status(401).json({ errors: defaultMessages["user-unregistered"] });
+        if (typeof nome !== "undefined") usuario.nome = nome;
+        if (typeof email !== "undefined") usuario.email = email;
+        if (typeof password !== "undefined") usuario.setSenha(password);
+        return usuario.save().then(() => {
+          return res.json({ usuario: usuario.enviarAuthJSON() });
+        }).catch(next);
+      }).catch(next);
+  }
 
+  // DELETE /
+  remove(req, res, next) {
+    Usuario.findById(req.payload.id).then(usuario => {
+      if (!usuario) return res.status(401).json({ errors: defaultMessages["user-unregistered"] });
+      return usuario.remove().then(() => {
+        return res.json({ deletado: true })
+      }).catch(next);
+    }).catch(next);
+  }
 
-
-
-
+  // POST /login
+  login(req, res, next) {
+    const { email, password } = req.body;
+    if (!email) return res.status(422).json({ errors: { email: defaultMessages["parcial-vazio"] } });
+    if (!password) return res.status(422).json({ errors: { password: defaultMessages["parcial-vazio"] } });
+    Usuario.findOne({ email }).then((usuario) => {
+      if (!usuario) return res.status(401).json({ errors: defaultMessages["user-unregistered"] });
+      if (!usuario.validarSenha(password)) return res.status(401).json({ errors: defaultMessages["invalid-password"] })
+      return res.json({ usuario: usuario.enviarAuthJSON() })
+    }).catch(next)
+  }
 
 
 }
